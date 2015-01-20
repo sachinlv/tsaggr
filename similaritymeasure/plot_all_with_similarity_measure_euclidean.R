011f9b6abb63d28fbc3244df1d5e390aff9b864e
sachinlv <nicaslv@gmail.com>
2015-01-20 10:11:49 +0100

    Plots generic code

--- similaritymeasure/plot_all_with_similarity_measure_euclidean.R
+++ similaritymeasure/plot_all_with_similarity_measure_euclidean.R
@@ -2,9 +2,19 @@ require(RMySQL)
 require(ff)
 require(RSNNS)
 require(TSdist)
-algo <- 'gbm'
+algo <- 'neuralnet'
+sim.meas <- 'fourier'
+ip.type <- 'statistical'
 file.path.single <- paste('/home/freak/Dropbox/results/',algo,'_shortterm_simple/',sep="")
 file.path.all <- paste('/home/freak/Dropbox/results/',algo,'_shortterm_aggr/all/',sep="")
+plot.file <- paste('/home/freak/Programming/Thesis/results/plots/',
+                   algo,'_',
+                   ip.type,'_',
+                   sim.meas,'.pdf',sep="")
+result.file <- paste('/home/freak/Programming/Thesis/results/plots/combination/',
+                      algo,'_',
+                      ip.type,'_',
+                      sim.meas,'.csv',sep="")
 
 tables <- c('onshore_SITE_00002',
             'onshore_SITE_00003',
@@ -23,7 +33,10 @@ file.name.single <- paste(algo,'_shortterm_simple.csv',sep="")
 file.name.all <- paste(algo,'_shortterm_aggr_combi',sep="")
 powdata <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
 powdata.normalized <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
+threshold.rmse <<- 0.3
+threshold.dist <<- 0.3
 
+combination.result <<- c()
 
 file.list <- list.files(file.path.all, pattern="*.csv")
 drv = dbDriver("MySQL")
@@ -42,6 +55,7 @@ load.data <- function(){
 
 load.err.data <- function(file.no){
   rmse <<- c()
+
   if(file.no==1){
     file <- file.name.single
     err.tbl <- read.csv(file,sep = ',')
@@ -67,10 +81,10 @@ gen.dist.mat <- function(aggr.seq){
     for(j in seq(1, len)){
       i1 <- aggr.seq[i]
       j1 <- aggr.seq[j]
-      mat[i,j] <- euclideanDistance(powdata[,i1],powdata[,j1])#,2)#, n=(floor(length(powdata[,i1])/2)+1))
+      mat[i,j] <- fourierDistance(powdata[,i1],powdata[,j1],n=(floor(length(powdata[,i1])/2)+1))#2)
+      #mat[i,j] <- abs(mean(powdata[,i1]-powdata[,j1]))
     }
   }
-
   return(mat)
 }
 
@@ -86,8 +100,7 @@ get.err.dist.data <- function(){
     #dist.mat[upper.tri(dist.mat,diag=TRUE)] <- 0
     #lower.tri.cnt <- ((row.len*col.len)-diag.len)/2
     #dist <- sum(dist.mat)/lower.tri.cnt
-    dist <- sum(dist.mat)/((row.len+col.len)-diag.len)
-
+    dist <- sum(dist.mat)/((row.len*col.len)-diag.len)
     avg.dist <- c(avg.dist,dist)
   }
 
@@ -97,8 +110,14 @@ get.err.dist.data <- function(){
   else{
     avg.dist <- normalizeData(as.vector(avg.dist),type="0_1")
   }
+  #combination result set
+  for(i in seq(1,length(avg.dist))){
+    if(avg.dist[i] <= threshold.dist && rmse$err[i] <= threshold.rmse){
+      combination.result <<- c(combination.result, rmse$seq[i])
+    }
+  }
 
-  data.to.plot <- data.frame(err.rmse=rmse$err, eucd.dist=avg.dist)
+  data.to.plot <- data.frame(err.rmse=rmse$err, dist=avg.dist)
   return(data.to.plot)
 }
 
@@ -106,6 +125,7 @@ get.err.dist.data <- function(){
 
 plot.all <- function(){
   load.data()
+  #x11()
   par(mfrow=c(5,2))
   par(mar=c(0.5, 4.5, 0.5, 0.5))
   plot.window(xlim=c(0,300),ylim=c(0,1),asp=1)
@@ -113,33 +133,55 @@ plot.all <- function(){
   setwd(file.path.single)
   load.err.data(1)
   data.to.plot <- rmse$err
-  plot(data.to.plot,type="l",
-       ylim=c(0,1),col="red",
-       xlab=paste("Individuals"),ylab="Rmse vals",
+  plot(data.to.plot,
+       type="p",
+       ylim=c(0,1),
+       col="red",
+       xlab=paste("Individuals"),
+       ylab="Rmse vals",
        main="Individuals")
-  legend("topright", legend=c("rmse"),
-         col=c("red"), text.col=c("red"))
+  legend("topright",
+         legend=c("rmse"),
+         col=c("red"),
+         text.col=c("red"))
 
   #plot2-10
   setwd(file.path.all)
   for(i in seq(2,10)){
     load.err.data(i)
     data.to.plot <- get.err.dist.data()
-    typ <- if(i == 10) 'p' else 'l'
-    plot(data.to.plot$err.rmse,type=typ,
-         ylim=c(0,1),col="red",
-         xlab=paste("combination",i),ylab="Rmse val~distance",
-         main="Combination")
-    #if(i<10){
-    #  lines(data.to.plot$eucd.dist,col="blue")
-    #}
-    #else{
-    #  print(data.to.plot$eucd.dist)
-      points(data.to.plot$eucd.dist,,col="blue")
-    #}
-    legend("topright", legend=c("rmse","euclideanDistance"),
-                      col=c("red","blue"), text.col=c("red","blue"))
+    #typ <- if(i == 10) 'p' else 'l'
+    y <- data.to.plot$err.rmse
+    x <- data.to.plot$dist
+    d <- data.frame(y,x)
+    f <- formula('y~x')
+    plot(f,d,
+         main=cor(data.to.plot$err.rmse,data.to.plot$dist),
+         #type="p",
+         #col=c("red","blue"),
+         xlab=paste("rmse combination ",i),
+         ylab=paste("distance combination ",i))
+         #main="Combination")neuralnet_statistical_euclidean.pdf
+         #plot(data.to.plot$dist)
+         #if(i<10){
+         #  lines(data.to.plot$dist,col="blue")
+         #}
+         #else{
+         #  print(data.to.plot$dist)
+         #points(data.to.plot$dist,,col="blue")
+         #}
+         #legend("topright", legend=c("rmse","fourierDistance"),
+         #           col=c("red","blue"), text.col=c("red","blue"))
   }
+  dev.copy2pdf(file =plot.file)
+  #dev.off()
+  combination.result <- as.character(combination.result)
+  combination.result <- gsub("0","10",combination.result)
+  df <- c(paste("RMSE threshold", as.character(threshold.rmse)))
+  df <- c(df,paste("Similarity Threshold",as.character(threshold.dist)))
+  #combination.result <- cbind(numeric(0),combination.result)
+  df <- c(df, combination.result)
+  write.table(df, result.file)
 
 }
 
