7ec62ef8c90e4af0ece807b719f8985fbc6d9753
sachinlv <nicaslv@gmail.com>
2015-02-01 11:33:20 +0100

    hclust code for 3 methods added

--- clustering/hierarchial/brnn_hclust_size10_forecast.R
+++ clustering/hierarchial/brnn_hclust_size10_forecast.R
@@ -11,9 +11,15 @@ data.len <- 52560
 data.len.day <<- 144
 mat.size <<- 365
 window.size <- 10
+sim.meas <- 'pca'
+plot.file <- 'brnn_shortterm_hclust_aggr_plot.pdf'
 train.data.percent <- 0.7
 file.name.generic <<-'brnn_shortterm_hclust'
+forecast.aggr.err.file <<- 'brnn_shortterm_hclust_aggr.csv'
 filepath <<- '/home/freak/Programming/Thesis/results/results/brnn_shortterm_hclust/'
+aggr.err <<- matrix(0,ncol=6,nrow=sites.count,byrow=TRUE, dimnames=NULL)
+colnames(aggr.err) <- c('cut size', 'max clust size','rmse','mape','sse','mse')
+
 table.ip.type <- "random"#"specific"
 powdata <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
 powdata.normalized <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
@@ -66,9 +72,42 @@ loaddata <- function(){
 generate.distance.matrix <- function(){
   for(i in seq(1,sites.count)){
     for(j in seq(1, sites.count)){
-      dist.mat[i,j] <<- euclideanDistance(powdata[,i],powdata[,j])
-      dist.mat.norm[i,j] <<- euclideanDistance(powdata.normalized[,i],
-                                               powdata.normalized[,j])
+      data.mat <- data.frame(c1=powdata[,i], c2=powdata[,j])
+
+      dist.mat[i,j] <- switch(sim.meas,
+                              "pca"={
+                                f <- formula("~ c1 + c2")
+                                pc <- princomp(f,data=data.mat, cor=FALSE)
+                                score <- data.frame(pc$scores)
+                                euclideanDistance(score$Comp.1,score$Comp.2)
+                              },
+                              "euclidean"={
+                                euclideanDistance(data.mat$c1,data.mat$c2)
+                              },
+                              "correlation"={
+                                correlationDistance(data.mat$c1,data.mat$c2)
+                              },
+                              "manhattan"={
+                                manhattanDistance(data.mat$c1,data.mat$c2)
+                              },
+                              "minkowski"={
+                                minkowskiDistance(data.mat$c1,data.mat$c2,2)
+                              },
+                              "fourier"={
+                                fourierDistance(data.mat$c1,data.mat$c2,n=(floor(length(data.mat$c1)/2)+1))
+                              },
+                              "mean"={
+                                abs(mean(data.mat$c1-data.mat$c2))
+                              },
+                              "mahalanobis"={
+                                d <- matrix(NA,nrow=52560,ncol=2)
+                                #d[,1] <- powdata[,i1]
+                                #d[,2] <- powdata[,j1]
+                                #c <- cov(d[,1:2])#,method=c("pearson"))
+                                #m <- c(mean(d[,1]), mean(d[,2]))
+                                #mat[i,j] <- mahalanobis(d,m,c)
+                              }
+      )
     }
   }
 }
@@ -85,9 +124,11 @@ generate.hierarchial.cluster <- function(){
 generate.aggr.matrix <- function(size,vect){
   aggr.mat <<- matrix(0,nrow=52560,ncol=size)
   col.names <- c()
+  clust.sizes <- c()
   for(cut.no in seq(1,size)){
     indx.seq <- which(vect==cut.no)
     mat <- as.matrix(powdata[,c(indx.seq)])
+    clust.sizes <- c(clust.sizes,length(indx.seq))
     col.names <- c(col.names, paste('S',paste(indx.seq, collapse=""), sep=""))
     if(length(mat[1,])>1){
       aggr.mat[,cut.no] <<- rowSums(mat)
@@ -96,6 +137,7 @@ generate.aggr.matrix <- function(size,vect){
     }
   }
   colnames(aggr.mat) <<- col.names
+  return(max(clust.sizes))
 }
 
 predict.pow <- function(cut.size, aggrno, indx) {
@@ -104,6 +146,7 @@ predict.pow <- function(cut.size, aggrno, indx) {
     return
   }
   data.normalized <- normalizeData(as.vector(aggr.mat[,aggrno]),type="0_1")
+  normParms <<- getNormParameters(data.normalized)
   data.set <- as.vector(data.normalized[,1])
   indx.start <<- indx
   indx.end <<- indx.start + (window.size * data.len.day) - 1
@@ -120,8 +163,8 @@ predict.pow <- function(cut.size, aggrno, indx) {
     train.dataset.indx <- floor(data.len.day *  train.data.percent)
     test.dataset.indx <- train.dataset.indx + 1
     window.slide <- data.len.day - train.dataset.indx
-    data.mat.train <- data.frame(data.mat[1:train.dataset.indx,])
-    data.mat.test <- data.frame(data.mat[test.dataset.indx:data.len.day,])
+    data.mat.train <- data.mat[1:train.dataset.indx,]
+    data.mat.test <- data.mat[test.dataset.indx:data.len.day,]
 
     formula.set <- colnames(data.mat)
     y = formula.set[window.size]
@@ -135,9 +178,9 @@ predict.pow <- function(cut.size, aggrno, indx) {
                  mu=0.1,
                  mu_dec=0.1,
                  mu_max=1e10,
-                 change = 0.001,
+                 change = 0.01,
                  neurons=window.size,
-                 normalize=FALSE,
+                 normalize=TRUE,
                  verbose=FALSE,
                  Monte_Carlo = FALSE)
 
@@ -145,7 +188,7 @@ predict.pow <- function(cut.size, aggrno, indx) {
     data.train <<- c(data.train, data.mat.train[,window.size])
     data.test <<- c(data.test, data.mat.test[,window.size])
 
-    pred <- predict.brnn(out, data.mat.test[,1:window.size-1])
+    pred <- predict.brnn(out ,data.mat.test[, 1:window.size-1])
     data.out <<- c(data.out, pred)
 
     indx.start <<- indx.start + window.slide
@@ -158,13 +201,17 @@ predict.pow <- function(cut.size, aggrno, indx) {
 
   train.data <<- cbind(train.data, data.train)
   test.data <<-  cbind(test.data, data.test)
+  test.data.denorm <<- cbind(test.data.denorm, as.vector(denormalizeData(data.test, normParms)))
   output <<- cbind(output, data.out)
+  output.denorm <<- cbind(output.denorm, as.vector(denormalizeData(data.out, normParms)))
 }
 
 
-prediction.error <- function(cut.size){
+prediction.error <- function(cut.size,max.clust.size){
   parm.count <- 5
+
   file.name <- paste(file.name.generic,cut.size,'.csv',sep="")
+
   setwd(filepath)
 
   if(cut.size!=0){
@@ -183,19 +230,53 @@ prediction.error <- function(cut.size){
       #break
     }
     write.csv(err.data, file=file.name)
+
+    if(cut.size > 1){
+      test.data.sum <<- rowSums(test.data.denorm[,1:cut.size])
+      test.data.sum <<- normalizeData(test.data.sum, type="0_1")
+
+      output.sum <<- rowSums(output.denorm[,1:cut.size])
+      output.sum <<- normalizeData(output.sum, type="0_1")
+    }else{
+      test.data.sum <<- test.data.denorm[,1]
+      test.data.sum <<- normalizeData(test.data.sum, type="0_1")
+
+      output.sum <<- output.denorm[,1]
+      output.sum <<- normalizeData(output.sum, type="0_1")
+    }
+
+
+    err.rmse <- error(forecast=output.sum, true=test.data.sum,method="rmse")
+    err.mape <- error(forecast=output.sum, true=test.data.sum,method="mape")
+    err.sse <- error(forecast=output.sum, true=test.data.sum,method="sse")
+    err.mse <- error(forecast=output.sum, true=test.data.sum,method="mse")
+
+    err.row <- c(cut.size,max.clust.size, err.rmse, err.mape, err.sse, err.mse)
+    aggr.err[cut.size,] <<- err.row
   }
 }
 
-predict.for.aggrdata <- function(cut.size){
+predict.for.aggrdata <- function(cut.size,max.clust.size){
   slide.indx <- data.len - (history.length * data.len.day) + 1
   train.data <<- c()
   test.data <<- c()
+  test.data.denorm <<- c()
   output <<- c()
+  output.denorm <<- c()
 
   for(aggr.nr in seq(1,cut.size)){
     predict.pow(cut.size,aggr.nr,slide.indx)
   }
-  prediction.error(cut.size)
+  prediction.error(cut.size,max.clust.size)
+}
+
+plot.err.aggr <- function(){
+  setwd(filepath)
+  err.tbl <- data.frame(read.csv(forecast.aggr.err.file))
+  x <- err.tbl$max.clust.size
+  y <- err.tbl$rmse
+  plot(y~x)
+  dev.copy2pdf(file =plot.file)
 }
 
 predict.hclust.aggregates <- function(){
@@ -206,10 +287,13 @@ predict.hclust.aggregates <- function(){
 
   for(cut.size in seq(1,no.of.cuts)){
     cut.group.vec <- cut.tree.mat[,cut.size]
-    generate.aggr.matrix(cut.size,cut.group.vec)
-    predict.for.aggrdata(cut.size)
+    max.clust.size <- generate.aggr.matrix(cut.size,cut.group.vec)
+    predict.for.aggrdata(cut.size,max.clust.size)
     #break
   }
+  setwd(filepath)
+  write.csv(aggr.err,forecast.aggr.err.file)
+  plot.err.aggr()
 }
 
 predict.hclust.aggregates()
--- clustering/hierarchial/neuralnet_hclust_size10_forecast.R
+++ clustering/hierarchial/neuralnet_hclust_size10_forecast.R
@@ -12,8 +12,14 @@ data.len.day <<- 144
 mat.size <<- 365
 window.size <- 10
 train.data.percent <- 0.7
+sim.meas <- 'pca'
+plot.file <- 'neuralnet_shortterm_hclust_aggr_plot.pdf'
 file.name.generic <<-'neuralnet_shortterm_hclust'
+forecast.aggr.err.file <<- 'neuralnet_shortterm_hclust_aggr.csv'
 filepath <<- '/home/freak/Programming/Thesis/results/results/neuralnet_shortterm_hclust/'
+aggr.err <<- matrix(0,ncol=6,nrow=sites.count,byrow=TRUE, dimnames=NULL)
+colnames(aggr.err) <- c('cut size', 'max clust size','rmse','mape','sse','mse')
+
 table.ip.type <- "random"#"specific"
 powdata <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
 powdata.normalized <<- ff(NA, dim=c(data.len, sites.count), vmode="double")
@@ -66,9 +72,42 @@ loaddata <- function(){
 generate.distance.matrix <- function(){
   for(i in seq(1,sites.count)){
     for(j in seq(1, sites.count)){
-      dist.mat[i,j] <<- euclideanDistance(powdata[,i],powdata[,j])
-      dist.mat.norm[i,j] <<- euclideanDistance(powdata.normalized[,i],
-                                               powdata.normalized[,j])
+      data.mat <- data.frame(c1=powdata[,i], c2=powdata[,j])
+
+      dist.mat[i,j] <- switch(sim.meas,
+                         "pca"={
+                           f <- formula("~ c1 + c2")
+                           pc <- princomp(f,data=data.mat, cor=FALSE)
+                           score <- data.frame(pc$scores)
+                           euclideanDistance(score$Comp.1,score$Comp.2)
+                         },
+                         "euclidean"={
+                           euclideanDistance(data.mat$c1,data.mat$c2)
+                         },
+                         "correlation"={
+                           correlationDistance(data.mat$c1,data.mat$c2)
+                         },
+                         "manhattan"={
+                           manhattanDistance(data.mat$c1,data.mat$c2)
+                         },
+                         "minkowski"={
+                           minkowskiDistance(data.mat$c1,data.mat$c2,2)
+                         },
+                         "fourier"={
+                           fourierDistance(data.mat$c1,data.mat$c2,n=(floor(length(data.mat$c1)/2)+1))
+                         },
+                         "mean"={
+                           abs(mean(data.mat$c1-data.mat$c2))
+                         },
+                         "mahalanobis"={
+                           d <- matrix(NA,nrow=52560,ncol=2)
+                           #d[,1] <- powdata[,i1]
+                           #d[,2] <- powdata[,j1]
+                           #c <- cov(d[,1:2])#,method=c("pearson"))
+                           #m <- c(mean(d[,1]), mean(d[,2]))
+                           #mat[i,j] <- mahalanobis(d,m,c)
+                         }
+      )
     }
   }
 }
@@ -85,9 +124,11 @@ generate.hierarchial.cluster <- function(){
 generate.aggr.matrix <- function(size,vect){
   aggr.mat <<- matrix(0,nrow=52560,ncol=size)
   col.names <- c()
+  clust.sizes <- c()
   for(cut.no in seq(1,size)){
     indx.seq <- which(vect==cut.no)
     mat <- as.matrix(powdata[,c(indx.seq)])
+    clust.sizes <- c(clust.sizes,length(indx.seq))
     col.names <- c(col.names, paste('S',paste(indx.seq, collapse=""), sep=""))
     if(length(mat[1,])>1){
       aggr.mat[,cut.no] <<- rowSums(mat)
@@ -96,6 +137,7 @@ generate.aggr.matrix <- function(size,vect){
     }
   }
   colnames(aggr.mat) <<- col.names
+  return(max(clust.sizes))
 }
 
 predict.pow <- function(cut.size, aggrno, indx) {
@@ -104,6 +146,7 @@ predict.pow <- function(cut.size, aggrno, indx) {
     return
   }
   data.normalized <- normalizeData(as.vector(aggr.mat[,aggrno]),type="0_1")
+  normParms <<- getNormParameters(data.normalized)
   data.set <- as.vector(data.normalized[,1])
   indx.start <<- indx
   indx.end <<- indx.start + (window.size * data.len.day) - 1
@@ -162,14 +205,17 @@ predict.pow <- function(cut.size, aggrno, indx) {
 
   train.data <<- cbind(train.data, data.train)
   test.data <<-  cbind(test.data, data.test)
+  test.data.denorm <<- cbind(test.data.denorm, as.vector(denormalizeData(data.test, normParms)))
   output <<- cbind(output, data.out)
+  output.denorm <<- cbind(output.denorm, as.vector(denormalizeData(data.out, normParms)))
 }
 
 
-prediction.error <- function(cut.size){
+prediction.error <- function(cut.size,max.clust.size){
   parm.count <- 5
   #'neuralnet_shortterm_hclust'
   file.name <- paste(file.name.generic,cut.size,'.csv',sep="")
+
   setwd(filepath)
 
   if(cut.size!=0){
@@ -188,19 +234,53 @@ prediction.error <- function(cut.size){
       #break
     }
     write.csv(err.data, file=file.name)
+
+    if(cut.size > 1){
+      test.data.sum <<- rowSums(test.data.denorm[,1:cut.size])
+      test.data.sum <<- normalizeData(test.data.sum, type="0_1")
+
+      output.sum <<- rowSums(output.denorm[,1:cut.size])
+      output.sum <<- normalizeData(output.sum, type="0_1")
+    }else{
+      test.data.sum <<- test.data.denorm[,1]
+      test.data.sum <<- normalizeData(test.data.sum, type="0_1")
+
+      output.sum <<- output.denorm[,1]
+      output.sum <<- normalizeData(output.sum, type="0_1")
+    }
+
+
+    err.rmse <- error(forecast=output.sum, true=test.data.sum,method="rmse")
+    err.mape <- error(forecast=output.sum, true=test.data.sum,method="mape")
+    err.sse <- error(forecast=output.sum, true=test.data.sum,method="sse")
+    err.mse <- error(forecast=output.sum, true=test.data.sum,method="mse")
+
+    err.row <- c(cut.size,max.clust.size, err.rmse, err.mape, err.sse, err.mse)
+    aggr.err[cut.size,] <<- err.row
   }
 }
 
-predict.for.aggrdata <- function(cut.size){
+predict.for.aggrdata <- function(cut.size,max.clust.size){
   slide.indx <- data.len - (history.length * data.len.day) + 1
   train.data <<- c()
   test.data <<- c()
+  test.data.denorm <<- c()
   output <<- c()
+  output.denorm <<- c()
 
   for(aggr.nr in seq(1,cut.size)){
       predict.pow(cut.size,aggr.nr,slide.indx)
   }
-  prediction.error(cut.size)
+  prediction.error(cut.size,max.clust.size)
+}
+
+plot.err.aggr <- function(){
+  setwd(filepath)
+  err.tbl <- data.frame(read.csv(forecast.aggr.err.file))
+  x <- err.tbl$max.clust.size
+  y <- err.tbl$rmse
+  plot(y~x)
+  dev.copy2pdf(file =plot.file)
 }
 
 predict.hclust.aggregates <- function(){
@@ -211,10 +291,13 @@ predict.hclust.aggregates <- function(){
 
   for(cut.size in seq(1,no.of.cuts)){
     cut.group.vec <- cut.tree.mat[,cut.size]
-    generate.aggr.matrix(cut.size,cut.group.vec)
-    predict.for.aggrdata(cut.size)
+    max.clust.size <- generate.aggr.matrix(cut.size,cut.group.vec)
+    predict.for.aggrdata(cut.size,max.clust.size)
     #break
   }
+  setwd(filepath)
+  write.csv(aggr.err,forecast.aggr.err.file)
+  plot.err.aggr()
 }
 
 predict.hclust.aggregates()
